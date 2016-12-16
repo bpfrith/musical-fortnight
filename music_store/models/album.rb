994e218a0c5b53ddb92163828ec5871aad5f9440
Ben Frith <bpfrith@gmail.com>
2016-12-16 14:24:57 +0000

    planning done and basic functionality in Ruby and SQL

---
+++ music_store/models/album.rb
@@ -0,0 +1,61 @@
+require_relative( '../db/sql_runner' )
+
+class Album
+
+  attr_accessor :title, :quantity; :genre, :artist_id
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i unless options['id'].nil?
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i unless options ["artist_id"].nil?
+  end
+
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = #{@id};"
+    albums = SQLRunner.run(sql)
+    return albums.map { |order| Album.new(order) }
+  end
+
+  def artist()
+    sql = "SELECT * FROM artists WHERE id = {#artist_id};"
+    artist = SQLRunner.run(sql)
+    return Artist.new(artist)
+
+  def self.all()
+    sql = "SELECT * FROM albums;"
+    albums = SQLRunner.run(sql)
+    return albums.map { |album| Album.new(album) }
+  end
+
+  def save()
+    sql =
+    "
+    INSERT INTO albums
+    (title, genre, artist_id)
+    VALUES
+    ('#{@title}', '#{@genre}', #{@artist_id})
+    RETURNING *;
+    "
+    result = SQLRunner.run(sql)
+    @id = result[0]['id'].to_i
+  end
+
+  def update()
+    sql = "
+      UPDATE albums
+      SET (title, genre, artist_id) =
+      ('#{@title}', '#{@genre}', #{@artist_id})
+      WHERE id = #{@id}
+    ;"
+    result = SQLRunner.run(sql)
+    return result
+  end
+
+  def delete()
+    sql = "DELETE FROM albums WHERE id = {@id}"
+    SQLRunner.run(sql)
+  end
+
+end
---
+++ music_store/models/artist.rb
@@ -0,0 +1,61 @@
+require_relative( '../db/sql_runner' )
+
+require('pg')
+require_relative('album.rb')
+require_relative('sql_runner.rb')
+
+class Artist
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i unless options['id'].nil?
+    @name = options['name']
+  end
+
+  def albums()
+    sql = "SELECT * FROM albums WHERE artist_id = #{@id};"
+    albums = SQLRunner.run(sql)
+    return albums.map { |album| Album.new(album) }
+  end
+
+  def self.all()
+    sql = "SELECT * FROM artists;"
+    artists = SQLRunner.run(sql)
+    return artists.map { |artist| Artist.new(artist) }
+  end
+
+  def save()
+    sql =
+    "
+    INSERT INTO artists
+    (name)
+    VALUES
+    ('#{@name}')
+    RETURNING *;
+    "
+    result = SQLRunner.run(sql)
+    first_hash = result[0]
+    id = first_hash["id"]
+    @id = id.to_i
+  end
+
+  def update()
+    sql = "
+      UPDATE artits
+      SET (name) =
+      ('#{@name}')
+      WHERE id = #{@id}
+    ;"
+    result = SQLRunner.run(sql)
+    return result
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM artists WHERE id = #{id};"
+    result = SQLRunner.run(sql)
+    return Artist.new(result)
+  end
+
+end
