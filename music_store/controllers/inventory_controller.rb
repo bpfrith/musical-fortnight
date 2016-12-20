5938137766db7dd85e80ecd913cec0527d3c78a5
Ben Frith <bpfrith@gmail.com>
2016-12-20 23:52:54 +0000

    bed time

--- music_store/controllers/inventory_controller.rb
+++ music_store/controllers/inventory_controller.rb
@@ -4,12 +4,14 @@ require( 'pry' )
 require_relative( '../models/inventory.rb' )
 require_relative( '../models/artist.rb' )
 require_relative( '../models/album.rb' )
+require_relative( '../models/taglines.rb' )
 require_relative( '../public/images/links.rb' )
 
 
 get '/inventory' do
   @inventory = Album.all().sort { |album1, album2| album1.artist.name <=> album2.artist.name }
   @corner_pic = Images.corner_pic()
+  @colour = Taglines.colour()
   erb ( :"inventory/index" )
 end
 
